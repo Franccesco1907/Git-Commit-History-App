e522b16dcec2ba0c79791d47393f2ca892879bf8
Franccesco Michael Jaimes Agreda <70769418+Franccesco1907@users.noreply.github.com>
2023-07-24 01:14:20 -0500

    feat(backend): add github commit interfaces

---
+++ backend/src/commit/interfaces/index.ts
@@ -0,0 +1,6 @@
+export { Author } from './author.interface';
+export { CommitAuthor } from './commit-author.interface';
+export { CommitTree } from './commit-tree.interface';
+export { CommitVerification } from './commit-verification.interface';
+export { Commit } from './commit.interface';
+export { GithubCommit } from './github-commit.interface';
