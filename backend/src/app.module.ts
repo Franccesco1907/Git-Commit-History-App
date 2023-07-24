c0f4c55f3312f6987d634b9171aa2cd6bf2ebff7
Franccesco Michael Jaimes Agreda <70769418+Franccesco1907@users.noreply.github.com>
2023-07-24 01:55:16 -0500

    feat(backend): add providers and imports in app module
    validate .env with JOI, add filters and interceptors in providers

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,40 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CommitModule } from './commit/commit.module';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { AllExceptionFilter } from './common/filters/http-exception.filter';
+import { TimeOutInterceptor } from './common/interceptors/timeout.interceptor';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+        GITHUB_API_URL: Joi.string().required(),
+        GITHUB_OWNER: Joi.string().required(),
+        GITHUB_REPO: Joi.string().required(),
+        GITHUB_API_TOKEN: Joi.string().required(),
+      }),
+    }),
+    CommitModule
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TimeOutInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+],
 })
 export class AppModule {}
