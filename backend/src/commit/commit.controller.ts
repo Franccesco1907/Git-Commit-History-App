8459e962f546b6caa1b1c49af34b7aadd256030d
Franccesco Michael Jaimes Agreda <70769418+Franccesco1907@users.noreply.github.com>
2023-07-24 08:11:41 -0500

    docs(backend): add swagger
    documentation of commit controller

--- backend/src/commit/commit.controller.ts
+++ backend/src/commit/commit.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get, HttpCode, HttpStatus, Query } from '@nestjs/common';
+import { ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CommitService } from './commit.service';
 
+@ApiTags('commit')
 @Controller('commit')
 export class CommitController {
   constructor(private readonly commitService: CommitService) { }
 
   @Get()
   @HttpCode(HttpStatus.OK)
+  @ApiResponse({ status: HttpStatus.OK, description: 'The commits were retrieved.' })
+  @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR, description: 'The commits could not be retrieved.' })
+  @ApiQuery({ name: 'query', required: false, type: String })
   getCommits(@Query('query') query: string) {
     return this.commitService.getCommits(query);
   }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   
+  const config = new DocumentBuilder()
+  .setTitle('API')
+  .setDescription('Github Commit History')
+  .setVersion('1.0')
+  .addTag('GithubCommitHistory')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('docs', app, document);
+
   const configService = app.get(ConfigService);
   const port = configService.get<string>('PORT');
 
